04f258f9a2d18d9f2dbf165534a706f03d7ff22c
Omsod0404 <garciaomarucho@gmail.com>
2023-10-21 22:08:55 -0600

    Routes implemented

--- routes/hruser.js
+++ routes/hruser.js
@@ -3,4 +3,46 @@ const hruser = express.Router();
 const db = require('../config/database');
 const jwt = require('jsonwebtoken');
 
+hruser.post("/signin", async (req, res, next)=>{
+    const {hruser_name, hruser_mail, hruser_password} = req.body;
+
+    if (hruser_name && hruser_mail && hruser_password) {
+        let query = `INSERT INTO hrusers (hruser_name, hruser_mail, hruser_password) `;
+        query += `VALUES('${hruser_name}', '${hruser_mail}', '${hruser_password}')`;
+
+        const rows = await db.query(query);
+
+        if(rows.affectedRows == 1){
+            return res.status(200).json({code: 201, message: "Usuario registrado correctamente"});
+        }
+        return res.status(500).json({code: 500, message: "Ocurrio un error"});
+    }
+    return res.status(500).json({code: 500, message: "Campos incompletos"})
+});
+
+hruser.post("/login", async (req, res, next) =>{
+    const {hruser_mail, hruser_password} = req.body;
+    const query = `SELECT * FROM hrusers WHERE hruser_mail = '${hruser_mail}' AND hruser_password = '${hruser_password}'`;
+    const rows = await db.query(query);
+
+    if (hruser_mail && hruser_password) {
+        if(rows.length == 1){
+            const token = jwt.sign({
+                hruser_id: rows[0].hruser_id,
+                hruser_mail: rows[0].hruser_mail
+            }, "debugkey");
+            return res.status(200).json({code:200, message:token});
+        }
+        else{
+            return res.status(200).json({code: 401, message:"Usuario y/o contraseña incorrectos"})
+        }
+    }
+    return res.status(500).json({code:500, message:"Campos incompletos"});
+})
+
+hruser.get("/", async (req, res, next) => {
+    const rows = await db.query("SELECT * FROM hrusers")
+    return res.status(200).json({code:200, message: rows});
+})
+
 module.exports = hruser;
--- routes/humanresources.js
+++ routes/humanresources.js
@@ -2,4 +2,80 @@ const express = require('express');
 const humanresources = express.Router();
 const db = require('../config/database');
 
+humanresources.post("/", async (req, res, next) =>{
+    const {employee_name, employee_lastname, employee_number, employee_mail, employee_address} = req.body;
+
+    if (employee_name && employee_lastname && employee_number && employee_mail && employee_address) {
+        let query = "INSERT INTO employees (employee_name, employee_lastname, employee_number,"; 
+        query+= "employee_mail, employee_address)";
+        query+= `VALUES('${employee_name}', '${employee_lastname}', '${employee_number}',`
+        query+= `'${employee_mail}', '${employee_address}')`;
+
+        const rows = await db.query(query);
+        if (rows.affectedRows == 1) {
+            return res.status(201).json({code:201, message: "Empleado insertado correctamente"});
+        }  
+
+        return res.status(500).json({code:500, message:"Ocurrio un error"});
+    }
+    return res.status(500).json({code: 500, message: "Campos incompletos"})
+});
+
+humanresources.delete("/:id([0-9]{1,3})", async (req, res, next)=>{
+    const query = `DELETE FROM employees WHERE employee_id=${req.params.id}`;
+    const rows = await db.query(query);
+
+    if(rows.affectedRows == 1){
+        return res.status(200).json({code: 200, message: "Empleado borrado correctamente"});
+    }
+    return res.status(404).json({code: 404, message: "Empleado no encontrado"});
+});
+
+humanresources.put("/:id([0-9]{1,3})", async (req, res, next)=>{
+    const {employee_name, employee_lastname, employee_number, employee_mail, employee_address} = req.body;
+    
+    if (employee_name && employee_lastname && employee_number && employee_mail && employee_address) {
+        let query = `UPDATE employees SET employee_name='${employee_name}', employee_lastname='${employee_lastname}',`;
+        query+= `employee_number='${employee_number}', employee_mail='${employee_mail}', employee_address='${employee_address}'`;
+        query+= `WHERE employee_id=${req.params.id}`;
+
+        const rows = await db.query(query);
+        if (rows.affectedRows == 1) {
+            return res.status(200).json({code:200, message: "Datos actualizados correctamente"});
+        }  
+
+        return res.status(500).json({code:500, message:"Ocurrio un error"});
+    }
+    return res.status(500).json({code: 500, message: "Campos incompletos"})
+});
+
+humanresources.patch("/:id([0-9]{1,3})", async (req, res, next)=>{   
+    const datosActualizados = req.body;
+
+    if (req.params.id) {
+        let query = `UPDATE employees SET ? WHERE employee_id=${req.params.id}`;
+        const rows = await db.query(query, datosActualizados);
+        if (rows.affectedRows >= 1) {
+            return res.status(200).json({code:200, message: "Datos actualizados correctamente"});
+        } 
+        return res.status(500).json({code: 500, message: "Ocurrio un error"});
+    }
+    return res.status(500).json({code: 500, message: "Campos incompleto"}); 
+});
+
+//Obtener todo
+humanresources.get("/", async (req, res, next) => {
+    const data = await db.query("SELECT * FROM employees")
+    return res.status(200).json({code:200, message: data});
+})
+
+//Busqueda por nombre
+humanresources.get('/:name([A-Za-z]+)', async (req, res, next) =>{
+    const name = req.params.name;
+    const data = await db.query("SELECT * FROM employees WHERE UPPER(employee_name) = UPPER('"+name+"')");
+    (data != null) 
+        ? res.status(200).json({code:200, message: data})
+        : res.status(404).json({code:404, message: "Empleado no encontrado"});
+})
+
 module.exports = humanresources;
